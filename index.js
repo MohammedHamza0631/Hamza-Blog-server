01b3fd40a02d19b7770870fb66ce7b286e1f1462
Mohammed Hamza <211220035@nitdelhi.ac.in>
2024-03-13 16:34:52 +0530

    add vercel URL

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ env.config();
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: "https://hamza-blog-teal.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
